df231f5c3bb20d59ddac1484ec534a41b897c045
joshuaPZ <joshuaprieto8@gmail.com>
2025-03-04 20:20:16 -0500

    add: dependencias y fragmento del mapa con api de GOOGLE

--- app/src/main/java/com/example/gooter_proyecto/Principal_Mapa.kt
+++ app/src/main/java/com/example/gooter_proyecto/Principal_Mapa.kt
@@ -1,20 +1,26 @@
 package com.example.gooter_proyecto
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.gooter_proyecto.databinding.ActivityPrincipalMapaBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
 
 class Principal_Mapa : AppCompatActivity() {
+    private lateinit var binding: ActivityPrincipalMapaBinding
+    private lateinit var map: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_principal_mapa)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityPrincipalMapaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        createFragment()
+    }
+
+    private fun createFragment() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as? SupportMapFragment
+        mapFragment?.getMapAsync { googleMap ->
+            map = googleMap
         }
     }
 }

35b52594242954e1c182c855cd052962dde0225f
Joan <joan_orduz@javeriana.edu.co>
2025-03-04 20:41:54 -0500

    Add: cambios visuales y binding de login a mapa
    Falta implementar la lógica del login

--- app/src/main/java/com/example/gooter_proyecto/LoginUsuario.kt
+++ app/src/main/java/com/example/gooter_proyecto/LoginUsuario.kt
@@ -1,5 +1,6 @@
 package com.example.gooter_proyecto
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gooter_proyecto.databinding.ActivityLoginUsuarioBinding
@@ -10,5 +11,9 @@ class LoginUsuario : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityLoginUsuarioBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonLogin.setOnClickListener {
+            startActivity(Intent(baseContext, Principal_Mapa::class.java))
+        }
     }
 }

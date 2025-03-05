dfc2f3cfe39e2969f179ff2a6d74aebc7993720b
Joan Emmanuel <joan_orduz@javeriana.edu.co>
2025-03-04 19:38:17 -0500

    add: PantallaFechaInicio
    falta logica en kotlin

--- app/src/main/java/com/example/gooter_proyecto/EstadisticasFechaInicio.kt
+++ app/src/main/java/com/example/gooter_proyecto/EstadisticasFechaInicio.kt
@@ -5,13 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.gooter_proyecto.databinding.ActivityHomeBinding
+import com.example.gooter_proyecto.databinding.ActivityEstadisticasFechaInicioBinding
 
-class HomeActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityHomeBinding
+class EstadisticasFechaInicio : AppCompatActivity() {
+    private lateinit var binding : ActivityEstadisticasFechaInicioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityHomeBinding.inflate(layoutInflater)
+        binding=ActivityEstadisticasFechaInicioBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/gooter_proyecto/LoginUsuario.kt
+++ app/src/main/java/com/example/gooter_proyecto/LoginUsuario.kt
@@ -1,13 +1,10 @@
 package com.example.gooter_proyecto
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.gooter_proyecto.databinding.ActivityLoginUsuarioBinding
 
-class login_usuario : AppCompatActivity() {
+class LoginUsuario : AppCompatActivity() {
     private lateinit var binding: ActivityLoginUsuarioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

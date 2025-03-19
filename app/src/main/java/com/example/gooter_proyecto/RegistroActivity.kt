fc9a21a8236e20c268335fae0f564861c0b9a2fd
mfruiz1025 <melissafruizb@javeriana.edu.co>
2025-03-19 01:26:26 -0500

    Funcionalidad fecha en actividad registro

--- app/src/main/java/com/example/gooter_proyecto/RegistroActivity.kt
+++ app/src/main/java/com/example/gooter_proyecto/RegistroActivity.kt
@@ -1,20 +1,40 @@
 package com.example.gooter_proyecto
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gooter_proyecto.databinding.ActivityRegistroBinding
+import java.util.Calendar
 
 class RegistroActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegistroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.ingresarRegistro.setOnClickListener { 
+        binding.ingresarRegistro.setOnClickListener {
             startActivity(Intent(baseContext, HomeActivity::class.java))
         }
 
+        binding.dateFecha.isFocusable = false
+        binding.dateFecha.isClickable = true
+        binding.dateFecha.setOnClickListener { showDatePicker() }
+    }
+
+    private fun showDatePicker() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+            val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+            binding.dateFecha.setText(selectedDate)
+        }, year, month, day)
+
+        datePickerDialog.show()
     }
 }

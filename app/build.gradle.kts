d6203f096be73ef6dfbe13b2a3014bc89f6c981d
joshuaPZ <joshuaprieto8@gmail.com>
2025-04-29 10:56:13 -0500

    add: logica de autentificacion de login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-auth-ktx")
+    implementation ("com.google.android.gms:play-services-auth:20.6.0")
+
 }
--- app/src/main/java/com/example/gooter_proyecto/LoginUsuarioActivity.kt
+++ app/src/main/java/com/example/gooter_proyecto/LoginUsuarioActivity.kt
@@ -2,18 +2,85 @@ package com.example.gooter_proyecto
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gooter_proyecto.databinding.ActivityLoginUsuarioBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser // Opcional: si necesitas info del usuario
 
 class LoginUsuarioActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityLoginUsuarioBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityLoginUsuarioBinding.inflate(layoutInflater)
+        binding = ActivityLoginUsuarioBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        firebaseAuth = FirebaseAuth.getInstance()
         binding.buttonIngresar.setOnClickListener {
-            startActivity(Intent(baseContext, HomeActivity::class.java))
+            validarYAutenticarUsuario()
         }
+        binding.buttonCancel.setOnClickListener {
+            finish()
+        }
+        binding.botonBack.setOnClickListener {
+            finish()
+        }
+
+        val currentUser = firebaseAuth.currentUser
+             if (currentUser != null) {
+                 val intent = Intent(this, HomeActivity::class.java)
+           intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+             startActivity(intent)
+              finish()
+          }
+
     }
+
+    private fun validarYAutenticarUsuario() {
+        val email = binding.editTextEmail.text.toString().trim()
+        val password = binding.editTextPassword.text.toString().trim()
+
+        if (email.isEmpty()) {
+            binding.editTextEmail.error = "El correo es obligatorio"
+            binding.editTextEmail.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.editTextEmail.error = "Ingrese un correo válido"
+            binding.editTextEmail.requestFocus()
+            return
+        }
+
+        if (password.isEmpty()) {
+            binding.editTextPassword.error = "La contraseña es obligatoria"
+            binding.editTextPassword.requestFocus()
+            return
+        }
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user: FirebaseUser? = firebaseAuth.currentUser
+                    Toast.makeText(this, "Inicio de sesión exitoso.", Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(this, HomeActivity::class.java)
+
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                    finish()
+
+                } else {
+                    val errorMessage = task.exception?.message ?: "Error desconocido"
+                    Toast.makeText(
+                        baseContext,
+                        "Falló la autenticación: $errorMessage",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+    }
+
 }

36e3b36699032462ed628ce69651a183130c1274
Joan Emmanuel <joan_orduz@javeriana.edu.co>
2025-05-01 23:23:04 -0500

    Add: Dependencias migración mapa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,5 +60,8 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation ("com.google.firebase:firebase-auth-ktx")
     implementation ("com.google.android.gms:play-services-auth:20.6.0")
+    implementation("org.osmdroid:osmdroid-android:6.1.18")
+    implementation("com.github.MKergall:osmbonuspack:6.8.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
 
 }

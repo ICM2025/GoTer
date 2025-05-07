6b01c3ab6b683b9f0710a73a3aea1a1067a063bf
joshuaPZ <joshuaprieto8@gmail.com>
2025-05-06 19:39:10 -0500

    add: Adaptar el canal para traer los datos de la base de datos

--- app/src/main/java/models/Canal.kt
+++ app/src/main/java/models/Canal.kt
@@ -1,8 +1,32 @@
 package models
 
-class Canal (nom: String, imagen: Int, seguido: Boolean, numMiembros: Int) {
-    val nombre: String = nom
-    val imagen: Int = imagen
-    var seguido: Boolean = seguido
-    var miembros: Int = numMiembros
+data class Canal(
+    val id: String,
+    val nombre: String,
+    val imagen: Int,
+    var seguido: Boolean = false,
+    var miembros: Int = 0,
+    val administrador: String = ""
+) {
+
+    constructor(nom: String, imagen: Int, seguido: Boolean, numMiembros: Int) :
+            this(
+                id = "",
+                nombre = nom,
+                imagen = imagen,
+                seguido = seguido,
+                miembros = numMiembros
+            )
+
+    // Constructor para crear desde datos de Firebase
+    constructor(id: String, canalData: Map<String, Any>) : this(
+        id = id,
+        nombre = canalData["nombreGrupo"] as? String ?: "",
+        imagen = 0,
+        seguido = false,
+        miembros = (canalData["miembros"] as? List<*>)?.size ?: 0,
+        administrador = canalData["administrador"] as? String ?: ""
+    )
+
+    
 }
